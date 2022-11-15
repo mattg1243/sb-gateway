93cb6f6079ac326403eaafde09a76d95e32a8ed0
Matt Gallucci <mattgallucci97@gmail.com>
2022-11-14 20:36:44 -0800

    proxies in place

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,25 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import indexRouter from './routes';
+import cors from 'cors';
+import proxy from 'express-http-proxy';
 
 dotenv.config();
 
-const PORT = process.env.PORT || 3001;
+// load env variables
+const USER_HOST = process.env.USER_HOSTNAME || 'http://localhost:8080';
+const AUTH_HOST = process.env.AUTH_HOSTNAME || 'http://localhost:8081';
+const PORT = process.env.PORT || 8000;
+// create express app
 const app = express();
-
+// middleware
+app.use(cors());
+// routes
 app.use(indexRouter);
-
+// microservice proxy routes
+app.use('/user', proxy(USER_HOST));
+app.use('/auth', proxy(AUTH_HOST));
+// start server
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+  console.log(`Gateway server listening on port ${PORT}...`);
 });
--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from "express"
 
 export const indexHandler = (req: Request, res: Response, next: NextFunction) => {
-  return res.status(200).json({message: 'server online!'});
+  return res.status(200).json({message: 'Gateway server online!'});
 }
